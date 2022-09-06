6115376ad27612e65ac116b69b089c9a018cb866
gonCSgit <103418696+gonCSgit@users.noreply.github.com>
2022-09-06 11:29:54 +0100

    fixing login functionality

--- src/Pages/SignIn.tsx
+++ src/Pages/SignIn.tsx
@@ -56,13 +56,16 @@ export default function SignIn(props: {
     }
 
     const data = new FormData(event.currentTarget);
-    const request = await fetch('https://classroom-mu.vercel.app/auth/signin', {
-      headers: {
-        'content-type': 'application/json',
+    const request = await fetch(
+      'https://classroom-backend-psi.vercel.app/auth/signin',
+      {
+        headers: {
+          'content-type': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify(Object.fromEntries(data)),
       },
-      method: 'POST',
-      body: JSON.stringify(Object.fromEntries(data)),
-    });
+    );
     const response = await JSON.parse(await request.text());
     console.log(response);
 
